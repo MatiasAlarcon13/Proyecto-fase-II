33a47852d6b77d197c84f059f4e55331af933b1c
Sabrina Vallejos Antunez <vallejosantunezs@gmail.com>
2026-06-18 20:03:43 -0300

    Patron Singleton implementado en la clase JPAUtil

--- src/main/java/laboratorio/Persistencia/JPAUtil.java
+++ src/main/java/laboratorio/Persistencia/JPAUtil.java
@@ -2,13 +2,35 @@ package laboratorio.Persistencia;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class JPAUtil { //crea la conexion con la base de datos
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("MiUnidadPersistencia");
+    private static final Logger logger = Logger.getLogger(JPAUtil.class.getName()); //logger que clasfica el error
+    private static final EntityManagerFactory emf; // el 'final' declara que nadie puede modificar o destruir el valor almacenado
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory("Persistencia");
+        } catch (Throwable ex) {
+            logger.log(Level.SEVERE, "Error critico al inicializar el EntityManagerFactory", ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    //constructor privado - no se puede instanciar esta clase por fuera de la misma
+    private JPAUtil() {
+        throw new IllegalStateException("Clase utilitaria - No se debe instanciar");
+    }
+
     public static EntityManagerFactory getEntityManagerFactory() {
         return emf;
     }
+
     public static void close(){
-        emf.close();
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
     }
 }
